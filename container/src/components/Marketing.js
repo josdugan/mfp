2c6d3b4f71611e47e256913b019f6c1552909a48
josdugan <josdugan@gmail.com>
2021-11-02 23:57:39 -0400

    Synchronize routing between microfrontends

--- container/src/components/Marketing.js
+++ container/src/components/Marketing.js
@@ -1,12 +1,25 @@
-import { mount } from 'marketing/Marketing';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { mount } from 'marketing/Marketing';
 
 const Marketing = () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
+
+        history.listen(onParentNavigate);
+
+    }, []);
 
     return (
         <div ref={ref} />
