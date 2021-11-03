9ae04fd92b6ee125e3e8e1418208664a8ed652d3
josdugan <josdugan@gmail.com>
2021-11-02 22:29:51 -0400

    Add css classname prefix

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName  } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Marketing from './components/Marketing';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <Marketing />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName} >
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Marketing />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
 
